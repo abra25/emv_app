5df0463ee64e1cd791e5222c44056ee1e42f614f
abra25 <abramkuja24@gmail.com>
2025-11-27 05:34:19 +0300

    improved css view

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -9,27 +9,76 @@ import { Router } from '@angular/router';
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
-export class EmployeeListComponent implements OnInit{
-user!:User[]
+export class EmployeeListComponent implements OnInit {
+  users: User[] = [];
+  filteredUsers: User[] = [];
+  pagedUsers: User[] = [];
 
-  constructor(private userService:UserService,private router: Router){}
+  searchTerm: string = '';
+  genderFilter: string = '';
+
+  currentPage: number = 1;
+  pageSize: number = 5; // number of users per page
+  totalPages: number = 1;
+
+  constructor(private userService: UserService, private router: Router) {}
+
   ngOnInit(): void {
-    this.getAllUser()
+    this.getAllUsers();
   }
-  public getAllUser(){
-    this.userService.getAllUsers().subscribe(data=>{
-       this.user= data;
-    })
+
+  getAllUsers() {
+    this.userService.getAllUsers().subscribe(data => {
+      this.users = data;
+      this.filterUsers();
+    });
+  }
+
+  filterUsers() {
+    let temp = this.users.filter(user =>
+      user.fullName.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      user.username.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      user.email.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+
+    if (this.genderFilter) {
+      temp = temp.filter(user => user.gender === this.genderFilter);
+    }
+
+    this.filteredUsers = temp;
+    this.currentPage = 1;
+    this.updatePagedUsers();
+  }
+
+  updatePagedUsers() {
+    this.totalPages = Math.ceil(this.filteredUsers.length / this.pageSize);
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    this.pagedUsers = this.filteredUsers.slice(startIndex, startIndex + this.pageSize);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePagedUsers();
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagedUsers();
+    }
   }
 
   updateUser(id: number) {
     this.router.navigate(['/update-user', id]);
   }
-  
+
   deleteUser(id: number) {
     this.userService.deleteUser(id).subscribe({
       next: () => {
-        this.user = this.user.filter(u => u.id !== id);
+        this.users = this.users.filter(u => u.id !== id);
+        this.filterUsers();
       },
       error: (err) => {
         console.error('Delete failed:', err);
@@ -37,5 +86,4 @@ user!:User[]
       }
     });
   }
-  
 }
