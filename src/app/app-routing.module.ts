835a45c44272e936ee801810e18dd972a8b74e44
abra25 <abramkuja24@gmail.com>
2025-11-27 09:45:18 +0300

    updated app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,20 +14,20 @@ import { ProfileComponent } from './components/profile/profile.component';
 const routes: Routes = [
   {path:'',component:LoginComponent},
   {
-    path: 'https://emplyeevacation.netlify.app/admin',
+    path: '/admin',
     component: DashboardComponent,
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'https://emplyeevacation.netlify.app/dashboard', component: EmployeeListComponent },  
-      { path: 'https://emplyeevacation.netlify.app/employees', component: EmployeeListComponent },
-      { path: 'https://emplyeevacation.netlify.app/vacation', component: VacationComponent }
+      { path: '/dashboard', component: EmployeeListComponent },  
+      { path: '/employees', component: EmployeeListComponent },
+      { path: '/vacation', component: VacationComponent }
     ]
   },
-  {path:'https://emplyeevacation.netlify.app/employee',component:EmployeeComponent},
-  {path:'https://emplyeevacation.netlify.app/register',component:RegisterComponent},
-  { path: 'https://emplyeevacation.netlify.app/update-user/:id', component: UpdateUserComponent },
+  {path:'/employee',component:EmployeeComponent},
+  {path:'/register',component:RegisterComponent},
+  { path: '/update-user/:id', component: UpdateUserComponent },
   { path: '**', redirectTo: '' } ,// Wildcard route for a 404 page 
-  { path: 'https://emplyeevacation.netlify.app/profile', component:ProfileComponent}
+  { path: '/profile', component:ProfileComponent}
 
 ];
 
